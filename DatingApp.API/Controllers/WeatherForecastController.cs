5a51832b7dc01986eae9c24b2b1d80c232b839df
Mohit <lohani.mohit96@gmail.com>
2020-08-08 00:07:04 +0530

    Intital Commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            this._context = context;
+
+        }
+        public async Task<IActionResult> Get()
+        {
+            var data= await _context.Values.ToListAsync();
+
+              return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var d= await _context.Values.Where(s=>s.Id==id).FirstOrDefaultAsync();
+
+            if(d==null){
+              return NotFound("Data not found");
+            }
+
+            return Ok(d);
+        }
+    }
+}
